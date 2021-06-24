25048dd5ae3c2f64416807d11f47135afeaee257
NatSilva <nathansilva250499@gmail.com>
2021-06-24 06:28:41 -0300

    Atualização de segurança

--- birdv2/src/pages/DepoisLogin.js
+++ birdv2/src/pages/DepoisLogin.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 import "./../Css/DepoisLogin.css"
-import { Route, Switch, Link } from 'react-router-dom';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Switch, Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
+const { SubMenu } = Menu;
 function DepoisLogin(props){
     const [cidade,setCidade] = useState("")
     const [estado,setEstado] = useState("")
@@ -14,22 +14,64 @@ function DepoisLogin(props){
         setNome(props.location.Nome)
 
     }
-    function MoradorCad(){
-
-    }
+    state = {
+        theme: 'dark',
+        current: '1',
+      };
+    
+      changeTheme = value => {
+        this.setState({
+          theme: value ? 'dark' : 'light',
+        });
+      };
+    
+      handleClick = e => {
+        this.setState({
+          current: e.key,
+        });
+      };
     return(
+        <>
         <div className="FundoDPL">
-            <div className="BarraMenu">
-                <p>Perfil</p>
-                <Link to="CadastroMorador" style={{color:'white', textDecoration:"none"}} onClick={MoradorCad}><p>Cadastrar Morador</p></Link>
-                <p>Cadastrar Funcionario</p>
-                <p>Cadastrar Aviso</p>
-                <p>Sobre</p>
-            </div>
-            <div className="Mostrar">
-                
-            </div>
+        <Switch
+          checked={this.state.theme === 'dark'}
+          onChange={this.changeTheme}
+          checkedChildren="Dark"
+          unCheckedChildren="Light"
+        />
+        <br />
+        <br />
+        <Menu
+          theme={this.state.theme}
+          onClick={this.handleClick}
+          style={{ width: 256 }}
+          defaultOpenKeys={['sub1']}
+          selectedKeys={[this.state.current]}
+          mode="inline"
+        >
+          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+            <Menu.Item key="3">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.Item key="11">Option 11</Menu.Item>
+            <Menu.Item key="12">Option 12</Menu.Item>
+          </SubMenu>
+        </Menu>
         </div>
+        </>
     )
 }
 
